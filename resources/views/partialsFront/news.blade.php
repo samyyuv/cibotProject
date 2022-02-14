90ae68c8d2d1d0bde086d45395a8a505edd3dfca
samyyuv <samyy.uv@gmail.com>
2022-02-14 19:37:26 +0100

    finishing actualités front for desktop and mobile

---
+++ resources/views/partialsFront/news.blade.php
@@ -0,0 +1,102 @@
+<section class="actualites">
+  <h1>actualités</h1>
+
+  <div class="actualites-btn">
+    <button>Toutes les actualités</button>
+    <button>Dernières actus</button>
+    <button>Expo manifestations</button>
+    <button>œuvres en cours</button>
+  </div>
+  <div class="actualites-container">
+    <div class="actualites-container-topic">
+      <div>
+        <img src="https://picsum.photos/350" alt="">
+        <div class="cross">
+          <a href=""> <span></span> <span></span></a>
+        </div>
+      </div>
+      <p>Date</p>
+      <h3>Titre œuvre</h3>
+      <p>Curabitur aliquet quam id dui posuere blandit. Cras ultricies ligula sed magna dictum porta.</p>
+    </div>
+    <div class="actualites-container-topic">
+      <div>
+        <img src="https://picsum.photos/350" alt="">
+        <div class="cross">
+          <a href=""> <span></span> <span></span></a>
+        </div>
+      </div>
+      <p>Date</p>
+      <h3>Titre œuvre</h3>
+      <p>Curabitur aliquet quam id dui posuere blandit. Cras ultricies ligula sed magna dictum porta.</p>
+    </div>
+    <div class="actualites-container-topic">
+      <div>
+        <img src="https://picsum.photos/350" alt="">
+        <div class="cross">
+          <a href=""> <span></span> <span></span></a>
+        </div>
+      </div>
+      <p>Date</p>
+      <h3>Titre œuvre</h3>
+      <p>Curabitur aliquet quam id dui posuere blandit. Cras ultricies ligula sed magna dictum porta.</p>
+    </div>
+    <div class="actualites-container-topic">
+      <div>
+        <img src="https://picsum.photos/350" alt="">
+        <div class="cross">
+          <a href=""> <span></span> <span></span></a>
+        </div>
+      </div>
+      <p>Date</p>
+      <h3>Titre œuvre</h3>
+      <p>Curabitur aliquet quam id dui posuere blandit. Cras ultricies ligula sed magna dictum porta.</p>
+    </div>
+    <div class="actualites-container-topic">
+      <div>
+        <img src="https://picsum.photos/350" alt="">
+        <div class="cross">
+          <a href=""> <span></span> <span></span></a>
+        </div>
+      </div>
+      <p>Date</p>
+      <h3>Titre œuvre</h3>
+      <p>Curabitur aliquet quam id dui posuere blandit. Cras ultricies ligula sed magna dictum porta.</p>
+    </div>
+    <div class="actualites-container-topic">
+      <div>
+        <img src="https://picsum.photos/350" alt="">
+        <div class="cross">
+          <a href=""> <span></span> <span></span></a>
+        </div>
+      </div>
+      <p>Date</p>
+      <h3>Titre œuvre</h3>
+      <p>Curabitur aliquet quam id dui posuere blandit. Cras ultricies ligula sed magna dictum porta.</p>
+    </div>
+    <div class="actualites-container-topic">
+      <div>
+        <img src="https://picsum.photos/350" alt="">
+        <div class="cross">
+          <a href=""> <span></span> <span></span></a>
+        </div>
+      </div>
+      <p>Date</p>
+      <h3>Titre œuvre</h3>
+      <p>Curabitur aliquet quam id dui posuere blandit. Cras ultricies ligula sed magna dictum porta.</p>
+    </div>
+    <div class="actualites-container-topic">
+      <div>
+        <img src="https://picsum.photos/350" alt="">
+        <div class="cross">
+          <a href=""> <span></span> <span></span></a>
+        </div>
+      </div>
+      <p>Date</p>
+      <h3>Titre œuvre</h3>
+      <p>Curabitur aliquet quam id dui posuere blandit. Cras ultricies ligula sed magna dictum porta.</p>
+    </div>
+  </div>
+
+
+</section>
