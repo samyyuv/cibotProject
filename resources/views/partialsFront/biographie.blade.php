db43ac438a7e8961e9abb081b27300eff226bafc
samyyuv <samyy.uv@gmail.com>
2022-02-15 18:54:59 +0100

    finishing biographie mobile and desktop

---
+++ resources/views/partialsFront/biographie.blade.php
@@ -0,0 +1,39 @@
+<section class="biographie">
+  <h1>biographie</h1>
+
+  <div class="biographie-btn">
+    <a class="biographie-title">Présentation <i class="fa-solid fa-chevron-down"></i> </a>
+    <ul class="biographie-dropdown">
+      <li><a>Présentation</a></li>
+      <li><a>Distinctions</a></li>
+      <li><a>Réalisations monumentales</a></li>
+      <li><a>Expositions personnelles</a></li>
+      <li><a>Expositions collectives</a></li>
+      <li><a>Événements</a></li>
+      <li><a>En permanence</a></li>
+    </ul>
+  </div>
+
+  <div class="biographie-presentation">
+    <div>
+      <img src="https://picsum.photos/406/341" alt="">
+      <h2>Présentation</h2>
+      <p>Née en 1960 dans une famille d’artistes collectionnant les bronzes de la Renaissance italienne,
+        la sculpture a toujours fait partie de l’environnement d’Élisabeth Cibot. Elle la retrouve avec bonheur
+        lors de ses études à l’ École Nationale Supérieure des Beaux-Arts de Paris, dans les ateliers d’Etienne Martin,
+        Léopold Kretz et César. Diplômée en 1983 (section burin), elle est également assistante du peintre Riccardo
+        Licata au Centro Internazionale di Grafica de Venise entre 1981 et 1983, puis « guest artist » auprès de la
+        Glass School de Harvey Littleton à Spruce Pine (U.S.A) en 1983. De retour à Paris, elle obtient un atelier de
+        la Ville en 1984. En parallèle, elle poursuit des études d’histoire de l’art et valide une première thèse en
+        1984 en Archéologie romaine, puis un DEA en 1990 en Histoire des Techniques. L’année 1993 marque un retour
+        à la statuaire de bronze et aux grands formats.</p>
+
+      <p>Depuis 1997, elle travaille à Nogent-sur-Marne dans un atelier de la Fondation Nationale pour les Arts Graphiques et
+        Plastiques. Ses œuvres se trouvent dans de nombreuses collections privées et publiques et sont présentées dans différentes
+        galeries internationales.</p>
+    </div>
+
+  </div>
+
+
+</section>
